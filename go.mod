e6c080ece1a33aa10be27f418304ca02c13843c8
Armando del Río <adrio.6180@gmail.com>
2025-02-23 23:03:57 -0600

    feat: add server with app and health endpont

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/circuit-shell/http-server-go
+
+go 1.23.6
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/healthz/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+    _,err:= w.Write([]byte(http.StatusText(http.StatusOK)))
+
+    if err!= nil{
+      log.Fatal(err)
+    }
+
+	})
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
